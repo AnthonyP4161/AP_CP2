eee63c152f3a4f8f46ba23955313cea2bf99a2ef
AnthonyP4161 <anthony.petersen@ucas-edu.net>
2026-01-20 09:52:29 -0700

    idk

---
+++ Individual Projects/library.py
@@ -0,0 +1,11 @@
+#AP 1st Period, Personal Library
+library = ()
+def view():
+    for i in library():
+        print(i)
+def add():
+    name = input("What is the name of the book: ")
+    author = input("What is the name of the auther")
+    library.add()
+def main():
+    print("Hiya, welcome to the personal library manager!\nHere you can create a library for all your books")
